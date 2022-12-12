6cb5380ea980f3099fcdd1990914837e896baabf
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2022-12-11 23:37:49 -0300

    Created Emitter Component files

---
+++ curso-angular/src/app/components/emitter/emitter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emitter',
+  templateUrl: './emitter.component.html',
+  styleUrls: ['./emitter.component.css']
+})
+export class EmitterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
