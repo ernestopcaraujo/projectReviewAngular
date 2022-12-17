ea26c50c4408e66830311ef48699434d2088bd6e
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2022-12-17 18:13:44 -0300

    Added code to run the event

--- curso-angular/src/app/components/emitter/emitter.component.ts
+++ curso-angular/src/app/components/emitter/emitter.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmitterComponent implements OnInit {
 
+  myNumber: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChangeNumber():number {
+    this.myNumber = Math.floor(Math.random()*100);
+    if(this.myNumber < 50){
+      this.myNumber = 99;
+    }
+    return this.myNumber;
   }
 
 }
