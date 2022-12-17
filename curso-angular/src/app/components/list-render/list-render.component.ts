6389f1114d0ead10cbf7d4045252525d40b90cee
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2022-12-17 19:02:07 -0300

    feat: created list-render component files

---
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
