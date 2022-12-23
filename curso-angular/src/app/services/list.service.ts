26e2ef115ab32cd00819313230b6ebb160caf432
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2022-12-23 03:39:54 -0300

    fix: Typing the service return

--- curso-angular/src/app/services/list.service.ts
+++ curso-angular/src/app/services/list.service.ts
@@ -8,11 +8,12 @@ export class ListService {
 
   constructor() { }
 
-  excludeCreditService(creditLines:ICredit[], creditLine:ICredit){
+  excludeCreditService(creditLines:ICredit[], creditLine:ICredit):ICredit[]{
     console.log(creditLine.operation);
     return creditLines.filter((cl)=>creditLine.operation != cl.operation);
   }
 
+  //Aqui eu estou forçando uma situação já que esses dados deveriam vir de um input de formulário
   includeCreditService(creditLines:ICredit[]){
 
     const creditLine:ICredit = {operation:'FGI', ltv:50};
