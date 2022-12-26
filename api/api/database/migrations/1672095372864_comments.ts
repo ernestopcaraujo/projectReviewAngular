4002920426a9a35eb26740959a84689bedb9e42b
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2022-12-26 20:14:48 -0300

    feat: created tables and relationship

--- api/api/database/migrations/1672095372864_comments.ts
+++ api/api/database/migrations/1672095372864_comments.ts
@@ -6,6 +6,16 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('username')
+      table.string('text')
+
+      //campo moment_id da tabela comments relaciona-se com o campo id da tabela moments
+      //se o moment "tal" tiver "n" comentários e houver uma deleção desse moment, com
+      //a atribuição de CASCADE ao onDelete() todos os comentários
+      //associados à este moment também serão excluídos.
+      //unsigned impõe que os valores no campo moment_id serão sempre positivos
+
+      table.integer('moment_id').unsigned().references('moments.id').onDelete("CASCADE")
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
