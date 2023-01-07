fd398ec6207a9635c874cbd1aaf07c48cad3f8fd
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-01-07 19:06:02 -0300

    feat: Added button dynamic text

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-moment-form',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent implements OnInit {
+
+  @Input() btnText!: string;
 
   constructor() { }
 
