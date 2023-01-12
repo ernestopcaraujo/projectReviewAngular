d4c37640c56585ae694cc868c9e0f2cddc50bcba
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-01-12 06:57:43 -0300

    feat: updated app.module for messages component

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     AboutComponent,
     HomeComponent,
     NewMomentComponent,
-    MomentFormComponent
+    MomentFormComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
