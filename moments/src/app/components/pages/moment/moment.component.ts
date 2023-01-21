5a190c899dae52fe271a3ed359458b75a513131e
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-01-21 13:58:34 -0300

    feat: added code to show moment

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/models/Moment';
 import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -11,6 +13,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class MomentComponent implements OnInit {
 
   moment?:Moment;
+  baseApiUrl = environment.baseApiUrl;
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(private momentService:MomentService,
               private route:ActivatedRoute) { }
