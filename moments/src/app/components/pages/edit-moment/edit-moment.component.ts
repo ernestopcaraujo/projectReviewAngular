58b955a266cd3e88690bd0f84cc94de4ff7cfe34
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-01-22 14:54:28 -0300

    feat: added eventHandler function

--- moments/src/app/components/pages/edit-moment/edit-moment.component.ts
+++ moments/src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/models/Moment';
 import { MomentService } from 'src/app/services/moment.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MessagesService } from 'src/app/services/messages.service';
 
 
 @Component({
@@ -15,14 +16,30 @@ export class EditMomentComponent implements OnInit {
   btnText: string ='Editar Momento'
 
   constructor(private mommentService:MomentService,
-              private route:ActivatedRoute) { }
+              private route:ActivatedRoute,
+              private messasgesService:MessagesService,
+              private router: Router) { }
 
   ngOnInit(): void {
     const id =Number(this.route.snapshot.paramMap.get("id"));
     this.mommentService.getMoment(id).subscribe(
       (item)=>this.moment = item.data
     );
+  }
 
+  async editHandler(momentData:Moment){
+    const id = this.moment.id;
+    const formData = new FormData();
+
+    formData.append('title', momentData.title);
+    formData.append('description', momentData.description);
+    if (momentData.image){
+      formData.append('image', momentData.image);
+    }
+    await this.mommentService.updateMoment(id!, formData).subscribe();
+    this.messasgesService.add(`Momento ${id} atualizado com sucesso !`);
+    this.router.navigate(['/']);
+    
   }
 
 }
