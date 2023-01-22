ca43dc781b70d4a363fbe6c5bd37b01d9d37233b
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-01-22 13:02:59 -0300

    feat: adding route to edit-moment component

--- moments/src/app/app-routing.module.ts
+++ moments/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { MomentComponent } from './components/pages/moment/moment.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'about', component:AboutComponent},
   {path:'moments/new', component:NewMomentComponent},
+  {path:'moments/edit/:id', component:EditMomentComponent},
   {path:'moments/:id', component:MomentComponent}
 ];
 
