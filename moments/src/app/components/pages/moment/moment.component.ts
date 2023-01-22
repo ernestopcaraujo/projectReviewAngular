851cb550c5c7336adf2bc32eb3456592b1ba278d
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-01-22 11:27:14 -0300

    feat: added remove function

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/models/Moment';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-moment',
@@ -18,13 +20,21 @@ export class MomentComponent implements OnInit {
   faEdit = faEdit;
 
   constructor(private momentService:MomentService,
-              private route:ActivatedRoute) { }
+              private route:ActivatedRoute,
+              private messagesService: MessagesService,
+              private router:Router) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.momentService.getMoment(id).subscribe((item)=>(this.moment = item.data));
     // iria ser usado o nome "moment" porém como está vindo o moment e mais a mensagem
     // optou-se por nomear como item
+  }
+
+  async removeHandler(id:number){
+    await this.momentService.removeMoment(id).subscribe();
+    this.messagesService.add("Momento removido com sucesso !");
+    this.router.navigate(['/']);
   }
 
 }
